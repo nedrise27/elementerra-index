5d772f1cfe0c4be75a0933614dd89372bf9d55aa
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-02-03 23:11:29 +0100

    fix deployment file

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -3,4 +3,11 @@
 cd ~/services/elementerra-index
 git pull
 npm run build
-/home/admin/.local/bin/pm2 restart ~/services/elementerra-index/dist/main.js 
+RELATIONAL_DATABASE_HOST='__RELATIONAL_DATABASE_HOST__' \
+  RELATIONAL_DATABASE_PORT='5432' \
+  RELATIONAL_DATABASE_USERNAME='__RELATIONAL_DATABASE_USERNAME__' \
+  RELATIONAL_DATABASE_PASSWORD='__RELATIONAL_DATABASE_PASSWORD__' \
+  RELATIONAL_DATABASE_NAME='__RELATIONAL_DATABASE_NAME__' \
+  /home/admin/.local/bin/pm2 \
+  restart ~/services/elementerra-index/dist/main.js \
+  --update-env
