5f9d09d17b34f5b38be7e59aa2e185c1784568d8
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-02-11 13:27:22 +0100

    no queue for websocket

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -1,33 +1,25 @@
 import {
-  OnGatewayConnection,
+  MessageBody,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import * as _ from 'lodash';
 import { Server } from 'ws';
-
-type FeedEvent = {
-  hash: string;
-  timestamp: number;
-  playerAddress: string;
-  event: string;
-};
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class EventsGateway implements OnGatewayConnection {
+export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  EVENTS_QUEUE: FeedEvent[] = [];
-  MAX_QUEUE_LENGTH = 100;
   EVENTS_TOPIC = 'events';
 
-  handleConnection(client: any) {
-    client.emit(this.EVENTS_TOPIC, this.EVENTS_QUEUE);
+  @SubscribeMessage('events')
+  handleEvent(@MessageBody() data: any) {
+    return data;
   }
 
   public async sendEvent(
@@ -35,36 +27,6 @@ export class EventsGateway implements OnGatewayConnection {
     playerAddress: string,
     event: string,
   ) {
-    const hash = `${timestamp}${playerAddress}${event}`;
-
-    if (this.addToQueue(hash, timestamp, playerAddress, event)) {
-      this.server.emit(this.EVENTS_TOPIC, { timestamp, playerAddress, event });
-    }
-  }
-
-  private addToQueue(
-    hash: string,
-    timestamp: number,
-    playerAddress: string,
-    event: string,
-  ) {
-    if (!this.EVENTS_QUEUE.find((e) => e.hash === hash)) {
-      if (
-        _.isEmpty(this.EVENTS_QUEUE) ||
-        _.last(this.EVENTS_QUEUE)!.timestamp < timestamp
-      ) {
-        this.EVENTS_QUEUE.push({ hash, timestamp, playerAddress, event });
-        this.EVENTS_QUEUE = _.orderBy(this.EVENTS_QUEUE, 'timestamp', 'desc');
-
-        if (this.EVENTS_QUEUE.length >= this.MAX_QUEUE_LENGTH) {
-          this.EVENTS_QUEUE = this.EVENTS_QUEUE.slice(0, this.MAX_QUEUE_LENGTH);
-        }
-
-        if (_.first(this.EVENTS_QUEUE).hash === hash) {
-          return true;
-        }
-      }
-    }
-    return false;
+    this.server.emit(this.EVENTS_TOPIC, { timestamp, playerAddress, event });
   }
 }
