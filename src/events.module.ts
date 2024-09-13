2b235d627771bf06a09c8d2113471cb6db116276
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-09-13 04:21:29 +0200

    fix build

--- src/events.module.ts
+++ src/events.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { EventsConfigurationModel } from './models/EventsConfiguration.model';
+import { Element } from './models';
 
 @Module({
-  imports: [SequelizeModule.forFeature([EventsConfigurationModel])],
+  imports: [SequelizeModule.forFeature([EventsConfigurationModel, Element])],
   providers: [EventsService],
   controllers: [],
   exports: [EventsService],
